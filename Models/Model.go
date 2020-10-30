2b714cbedc13941b7f1dd973444d9be734a1361c
FeuillatreThomas <Raphael41*>
2020-10-30 12:52:42 +0100

    initial commit

---
+++ Models/Model.go
@@ -0,0 +1,5 @@
+package Models
+
+import (
+	_ "github.com/go-sql-driver/mysql"
+)
---
+++ main.go
@@ -0,0 +1,27 @@
+package main
+
+import (
+	"TodoApp/Config"
+	"TodoApp/Models"
+	"TodoApp/Routes"
+	"fmt"
+	"github.com/jinzhu/gorm"
+)
+
+var err error
+
+func main() {
+	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+
+	if err != nil {
+		fmt.Println("status: ", err)
+	}
+
+	defer Config.DB.Close()
+
+	Config.DB.AutoMigrate(&Models.Todo{})
+
+	r := Routes.SetupRouter()
+
+	r.Run()
+}
