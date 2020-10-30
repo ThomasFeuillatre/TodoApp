5f6109f616710d63fab7c73396804b54b95c559f
FeuillatreThomas <Raphael41*>
2020-10-30 13:03:20 +0100

    add db url

--- main.go
+++ main.go
@@ -11,7 +11,7 @@ import (
 var err error
 
 func main() {
-	Config.DB, err = gorm.Open("mysql", Config.DbURL(Config.BuildDBConfig()))
+	Config.DB, err = gorm.Open("mysql", "mysql://pc86waepw158hqgw:s8uoz9qu4k5elrhj@ryfqldzbliwmq6g5.chr7pe7iynqr.eu-west-1.rds.amazonaws.com:3306/mjurrpat6gcq8pt1")
 
 	if err != nil {
 		fmt.Println("status: ", err)
